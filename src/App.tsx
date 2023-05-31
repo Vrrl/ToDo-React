0efb29c7fc1808d966ca62744bfdddb7a7dbcda1
vrrl <nykogabriel@gmail.com>
2023-05-30 23:41:15 -0300

    try to solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useEffect } from "react"
 import styles from './index.module.css'
 import { useNavigate } from "react-router-dom"
 import preview from "../public/preview.svg"
 
 export const App = () => {
     const navigate = useNavigate()
-
-    useEffect(() => {
-        document.body.style.backgroundColor = '#1E6F9F'
-
-        return () => {
-            document.body.style.backgroundColor = '#454545'
-        }
-    }, [])
 
     return (
         <div className={styles.main}>
